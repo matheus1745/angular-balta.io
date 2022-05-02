720d233e28e34dfea27cd9aee23239c7d647fa45
MatheusLima <matheuslima.musica@gmail.com>
2022-05-02 14:12:35 -0300

    second commit

--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -20,10 +20,21 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    
-    this.todos.push( new Todo(1, 'Passear com o cachorro', false) );
-    this.todos.push( new Todo (2, 'ir ao supermercado', false) );
-    this.todos.push( new Todo (3, 'cortar o cabelo', true) );
+
+    this.load();
+
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
@@ -31,14 +42,30 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
 }
