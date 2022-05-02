4a9a242f7328417389cc62a9d4fc2d02cdcfa25c
MatheusLima <matheuslima.musica@gmail.com>
2022-05-02 00:10:54 -0300

    first commit

---
+++ todoapp/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+    
+    this.todos.push( new Todo(1, 'Passear com o cachorro', false) );
+    this.todos.push( new Todo (2, 'ir ao supermercado', false) );
+    this.todos.push( new Todo (3, 'cortar o cabelo', true) );
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markUndone(todo: Todo) {
+    todo.done = false;
+  }
+
+}
